0784f8f612c5c2ca35dcde08c9ee73192a23134b
RMBDon <115331072+RMBDon@users.noreply.github.com>
2024-06-18 03:16:30 +0800

    Add files via upload

---
+++ assets/youtube.js
@@ -0,0 +1,17 @@
+// youtube.js
+document.addEventListener("DOMContentLoaded", function() {
+    const videoContainers = document.querySelectorAll('.video-container');
+    videoContainers.forEach(container => {
+        const videoId = container.dataset.videoId;
+        if (videoId) {
+            const iframe = document.createElement('iframe');
+            iframe.width = '560';
+            iframe.height = '315';
+            iframe.src = `https://www.youtube.com/embed/${videoId}`;
+            iframe.frameBorder = '0';
+            iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+            iframe.allowFullscreen = true;
+            container.appendChild(iframe);
+        }
+    });
+});
